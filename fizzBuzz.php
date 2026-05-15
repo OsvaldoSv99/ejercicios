b5deb2c04ed7c4d064da2b77bd576e1e07f9b84a
Osvaldo <osvaldo-mago99@hotmail.com>
2026-05-14 21:27:03 -0600

    fizzbuzz

---
+++ fizzBuzz.php
@@ -0,0 +1,17 @@
+<?php
+
+for ($i=1; $i <= 100; $i++) { 
+    if ((($i % 3) == 0) && (($i % 5) == 0)) {
+        echo $i . ' FizzBuzz<br>';
+    }else{
+        if (($i % 3) == 0) {
+            echo $i . ' Fizz <br>';
+        }elseif (($i % 5) == 0) {
+            echo $i .' Buzz <br>';
+        }
+        else{
+            echo $i .'<br>';
+        }
+    }    
+}
+?>
